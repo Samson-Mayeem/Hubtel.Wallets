562e37989127538d6d86a24975557a371efe7c3b
Samson-Mayeem <63527034+Samson-Mayeem@users.noreply.github.com>
2022-10-09 21:43:55 +0000

    Add project files.

---
+++ Hubtel.Wallets.Api/Models/Domain/Wallet.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hubtel.Wallets.Api.Models.Domain
+{
+    public class Wallet
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
